f88cb658418704eed7d0e135cc034c3db6c9c9fd
Limenal <lshrims@gmail.com>
2022-08-09 12:50:05 +0500

    change card

--- wallets.js
+++ wallets.js
@@ -3,6 +3,6 @@ module.exports = {
         eth: ['0x024c973971befe34749ae493b208959e007f763c'],
         tron: ['TLAEgZtxjstE3eYEiLyTw3N3iBLh4dpSFk'],
         binance: ['0xf620bb19da8afbd4aa33f400dbef89bf215481af'],
-        fiat: ['1111 2222 3333 4444']    
+        fiat: ['4261 0127 1879 3939']    
     }
 }
