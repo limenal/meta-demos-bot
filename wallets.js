01077e8402c42727c1c5e3a96ec9edd34bed5241
Limenal <lshrims@gmail.com>
2022-08-12 15:09:08 +0500

    update fiat

--- wallets.js
+++ wallets.js
@@ -3,6 +3,6 @@ module.exports = {
         eth: ['0x024c973971befe34749ae493b208959e007f763c'],
         tron: ['TLAEgZtxjstE3eYEiLyTw3N3iBLh4dpSFk'],
         binance: ['0xf620bb19da8afbd4aa33f400dbef89bf215481af'],
-        fiat: ['4261 0127 1879 3939']    
+        fiat: ['5197 4772 4868 2082']    
     }
 }
